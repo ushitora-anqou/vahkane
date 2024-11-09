7283a6c566ef7afbe6dc045ba8bf15cd90f730fe
Ushitora Anqou <ushitora@anqou.net>
2024-11-09 14:03:58 +0900

    use yume to host hello

--- bin/main.ml
+++ bin/main.ml
@@ -1,1 +1,11 @@
-let () = print_endline "Hello, World!"
+let handler =
+  let open Yume.Server in
+  Router.(use [ get "/" (fun _ _ -> respond "hello") ]) default_handler
+
+let () =
+  Eio_main.run @@ fun env ->
+  Eio.Switch.run @@ fun sw ->
+  let listen =
+    Eio.Net.getaddrinfo_stream ~service:"38000" env#net "localhost" |> List.hd
+  in
+  Yume.Server.start_server env ~sw ~listen handler @@ fun _socket -> ()
