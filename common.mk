c1f15f9290631efde7ffca95dd7287f11627739c
Ushitora Anqou <ushitora@anqou.net>
2024-11-10 12:32:16 +0900

    simplify generated stuff

---
+++ common.mk
@@ -0,0 +1,40 @@
+LOCALBIN ?= $(dir $(abspath $(lastword $(MAKEFILE_LIST))))/bin
+
+ENVTEST ?= $(LOCALBIN)/setup-envtest
+ENVTEST_VERSION ?= release-0.19
+ENVTEST_K8S_VERSION = 1.31.0
+
+IMG ?= controller:latest
+
+.PHONY: setup
+setup: envtest
+	@command -v aqua > /dev/null || { \
+		echo "Install aqua. See https://aquaproj.github.io/docs/install" ;\
+		exit 1 ;\
+	}
+	aqua i
+
+$(LOCALBIN):
+	mkdir -p $(LOCALBIN)
+
+.PHONY: envtest
+envtest: $(ENVTEST) ## Download setup-envtest locally if necessary.
+$(ENVTEST): $(LOCALBIN)
+	$(call go-install-tool,$(ENVTEST),sigs.k8s.io/controller-runtime/tools/setup-envtest,$(ENVTEST_VERSION))
+
+# go-install-tool will 'go install' any package with custom target and name of binary, if it doesn't exist
+# $1 - target path with name of binary
+# $2 - package url which can be installed
+# $3 - specific version of package
+define go-install-tool
+@[ -f "$(1)-$(3)" ] || { \
+set -e; \
+package=$(2)@$(3) ;\
+echo "Downloading $${package}" ;\
+rm -f $(1) || true ;\
+GOBIN=$(LOCALBIN) go install $${package} ;\
+mv $(1) $(1)-$(3) ;\
+} ;\
+ln -sf $(1)-$(3) $(1)
+endef
+
---
+++ test/e2e/e2e_suite.go
@@ -0,0 +1,14 @@
+package e2e_test
+
+import (
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+)
+
+var _ = Describe("controller", Ordered, func() {
+	Context("Operator", func() {
+		It("should run successfully", func() {
+			Expect(true).To(BeTrue())
+		})
+	})
+})
