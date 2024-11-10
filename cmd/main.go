c62c5c41ef9cfbbdc13c6d166faabc988f59216b
Ushitora Anqou <ushitora@anqou.net>
2024-11-10 21:19:19 +0900

    wip discord webhook server

--- cmd/main.go
+++ cmd/main.go
@@ -1,18 +1,29 @@
 package main
 
 import (
+	"context"
+	"crypto/ed25519"
 	"crypto/tls"
+	"encoding/hex"
+	"encoding/json"
+	"errors"
 	"flag"
+	"io"
+	"net/http"
 	"os"
+	"sync"
+	"time"
 
 	// Import all Kubernetes client auth plugins (e.g. Azure, GCP, OIDC, etc.)
 	// to ensure that exec-entrypoint and run can make use of them.
+	"github.com/go-logr/logr"
 	_ "k8s.io/client-go/plugin/pkg/client/auth"
 
 	"k8s.io/apimachinery/pkg/runtime"
 	utilruntime "k8s.io/apimachinery/pkg/util/runtime"
 	clientgoscheme "k8s.io/client-go/kubernetes/scheme"
 	ctrl "sigs.k8s.io/controller-runtime"
+	"sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/healthz"
 	"sigs.k8s.io/controller-runtime/pkg/log/zap"
 	"sigs.k8s.io/controller-runtime/pkg/metrics/filters"
@@ -56,6 +67,18 @@ func main() {
 	flag.Parse()
 
 	ctrl.SetLogger(zap.New(zap.UseFlagOptions(&opts)))
+
+	discordApplicationPublicKey, ok := os.LookupEnv("DISCORD_APPLICATION_PUBLIC_KEY")
+	if !ok {
+		msg := "set DISCORD_APPLICATION_PUBLIC_KEY"
+		setupLog.Error(errors.New(msg), msg)
+		os.Exit(1)
+	}
+	discordApplicationPublicKeyParsed, err := hex.DecodeString(discordApplicationPublicKey)
+	if err != nil {
+		setupLog.Error(err, "failed to parse DISCORD_APPLICATION_PUBLIC_KEY")
+		os.Exit(1)
+	}
 
 	// if the enable-http2 flag is false (the default), http/2 should be disabled
 	// due to its vulnerabilities. More specifically, disabling http/2 will
@@ -124,6 +147,18 @@ func main() {
 
 	// +kubebuilder:scaffold:builder
 
+	err = mgr.Add(
+		NewDiscordWebhookServerRunner(
+			mgr.GetClient(),
+			mgr.GetLogger().WithName("DiscordWebhookServerRunner"),
+			discordApplicationPublicKeyParsed,
+		),
+	)
+	if err != nil {
+		setupLog.Error(err, "unable to add DiscordWebhookServerRunner")
+		os.Exit(1)
+	}
+
 	if err := mgr.AddHealthzCheck("healthz", healthz.Ping); err != nil {
 		setupLog.Error(err, "unable to set up health check")
 		os.Exit(1)
@@ -138,4 +173,178 @@ func main() {
 		setupLog.Error(err, "problem running manager")
 		os.Exit(1)
 	}
+}
+
+type DiscordWebhookServerRunner struct {
+	k8sClient client.Client
+	logger    logr.Logger
+	publicKey ed25519.PublicKey
+}
+
+func NewDiscordWebhookServerRunner(
+	k8sClient client.Client,
+	logger logr.Logger,
+	publicKey ed25519.PublicKey,
+) *DiscordWebhookServerRunner {
+	return &DiscordWebhookServerRunner{
+		k8sClient: k8sClient,
+		logger:    logger,
+		publicKey: publicKey,
+	}
+}
+
+func (r *DiscordWebhookServerRunner) verifyRequest(header http.Header, body []byte) (bool, error) {
+	signatureEncoded := header.Get("X-Signature-Ed25519")
+	timestamp := header.Get("X-Signature-Timestamp")
+
+	message := make([]byte, len(timestamp)+len(body))
+	copy(message[0:], timestamp)
+	copy(message[len(timestamp):], body)
+
+	signature, err := hex.DecodeString(signatureEncoded)
+	if err != nil {
+		return false, err
+	}
+
+	return ed25519.Verify(r.publicKey, message, signature), nil
+}
+
+func respondJSON(w http.ResponseWriter, v interface{}) error {
+	json, err := json.Marshal(v)
+	if err != nil {
+		return err
+	}
+	w.Header().Add("Content-Type", "application/json")
+	if _, err := w.Write(json); err != nil {
+		return err
+	}
+	return nil
+}
+
+func (r *DiscordWebhookServerRunner) handleApplicationCommand(
+	w http.ResponseWriter,
+	body []byte,
+) error {
+	var req struct {
+		Data      interface{} `json:"data"`
+		GuildID   string      `json:"guild_id"`
+		ChannelID string      `json:"channel_id"`
+	}
+	if err := json.Unmarshal(body, &req); err != nil {
+		return err
+	}
+
+	var resp struct {
+		Type int `json:"type"`
+		Data struct {
+			Content string `json:"content"`
+		} `json:"data"`
+	}
+	resp.Type = 4
+	resp.Data.Content = "foobar"
+
+	if err := respondJSON(w, &resp); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (r *DiscordWebhookServerRunner) handleWebhook(
+	w http.ResponseWriter,
+	req *http.Request,
+) error {
+	// cf. https://discord.com/developers/docs/interactions/overview
+
+	defer func() {
+		_ = req.Body.Close()
+	}()
+	body, err := io.ReadAll(req.Body)
+	if err != nil {
+		return err
+	}
+	r.logger.Info("discord webhook request: " + string(body))
+
+	verified, err := r.verifyRequest(req.Header, body)
+	if err != nil {
+		return err
+	}
+	if !verified {
+		w.WriteHeader(http.StatusUnauthorized)
+		return nil
+	}
+
+	var root map[string]interface{}
+	if err := json.Unmarshal(body, &root); err != nil {
+		return err
+	}
+	requestType, ok1 := root["type"]
+	requestTypeParsed, ok2 := requestType.(float64)
+	if !ok1 || !ok2 {
+		return errors.New("type not found in the request")
+	}
+
+	switch int(requestTypeParsed) {
+	case 1: // PING
+		if err := respondJSON(w, map[string]int{"type": 1 /* PONG */}); err != nil {
+			return err
+		}
+
+	case 2: // APPLICATION_COMMAND
+		if err := r.handleApplicationCommand(w, body); err != nil {
+			return err
+		}
+
+	default:
+		r.logger.Info("unexpected request", "body", body)
+		w.WriteHeader(http.StatusNoContent)
+	}
+
+	return nil
+}
+
+func (r *DiscordWebhookServerRunner) Start(ctx context.Context) error {
+	mux := http.NewServeMux()
+	mux.HandleFunc("/webhook", func(w http.ResponseWriter, req *http.Request) {
+		if err := r.handleWebhook(w, req); err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			r.logger.Error(err, "failed to handle webhook request")
+			return
+		}
+	})
+
+	addr := "0.0.0.0:38000"
+	srv := http.Server{
+		Addr:           addr,
+		ReadTimeout:    10 * time.Second,
+		WriteTimeout:   10 * time.Second,
+		MaxHeaderBytes: 1 << 20,
+		Handler:        mux,
+	}
+
+	var wg sync.WaitGroup
+	defer wg.Wait()
+
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		r.logger.Info("starting discord webhook server", "addr", addr)
+		if err := srv.ListenAndServe(); err != nil {
+			r.logger.Error(err, "failed to start http server")
+		}
+	}()
+
+	<-ctx.Done()
+
+	ctxShutdown, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+	if err := srv.Shutdown(ctxShutdown); err != nil {
+		r.logger.Error(err, "failed to shutdown http server")
+	}
+
+	return nil
+}
+
+func (r DiscordWebhookServerRunner) NeedLeaderElection() bool {
+	return true
 }
